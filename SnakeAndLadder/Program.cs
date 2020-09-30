b39cf5de0699ee6d36e03818a09574fb2553619c
schakravarti <schakravarti930@gmail.com>
2020-09-30 21:37:41 +0530

    [Swapnil] Add : Method For Rolling Dice

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,16 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        public const int START_POS = 0, FINISH_POS = 100;
+        public static Random random = new Random();
+        public static int DiceRoll()
+        {
+            return random.Next(1, 7);
+        }
         static void Main(string[] args)
         {
-            const int START_POS = 0, FINISH_POS = 100;
+            int currentPosition = START_POS;
+            int numOnDice = DiceRoll();
         }
     }
 }
