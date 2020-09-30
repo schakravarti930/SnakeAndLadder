42d2418c94602dd9128cdaa12e95cc65fa0728bd
schakravarti <schakravarti930@gmail.com>
2020-09-30 11:12:50 +0530

    [Swapnil] Add : Added starting position

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int START_POS = 0, FINISH_POS = 100;
+        }
+    }
+}
